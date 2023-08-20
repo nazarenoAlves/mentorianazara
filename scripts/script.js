71354515e36d2caa7fde60b86193820cddcb8ed0
Nazareno Alves <nazarenoalvesdacosta@gmail.com>
2023-08-20 20:56:55 -0300

    feat: Novas fotos, Slide de depoimentos

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,26 @@
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+   direction: 'horizontal',
+   loop: true,
+ 
+   // If we need pagination
+   pagination: {
+     el: '.swiper-pagination',
+   },
+ 
+   // Navigation arrows
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+ 
+   // And if we need scrollbar
+   scrollbar: {
+     el: '.swiper-scrollbar',
+   },
+ });
+ 
+
 window.addEventListener('scroll', function() {
     var fadeInElements = document.querySelectorAll('.fade');
     for (var i = 0; i < fadeInElements.length; i++) {
@@ -9,4 +32,6 @@ window.addEventListener('scroll', function() {
        }
     }
  });
+
+
  
